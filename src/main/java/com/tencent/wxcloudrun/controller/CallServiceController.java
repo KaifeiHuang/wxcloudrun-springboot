20c58fe879ab7904980a76451902e29c05b8ee09
huangkaifei <huangkaife.hkf@antgroup.com>
2023-04-13 17:33:07 +0800

    call service api

---
+++ src/main/java/com/tencent/wxcloudrun/controller/CallServiceController.java
@@ -0,0 +1,49 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2023 All Rights Reserved.
+ */
+package com.tencent.wxcloudrun.controller;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+/**
+ * @author huangkaifei
+ * @version : CallServiceController.java, v 0.1 2023年04月13日 4:23 PM huangkaifei Exp $
+ */
+@Slf4j
+@RestController
+@RequestMapping("/callService")
+public class CallServiceController {
+
+    @GetMapping()
+    public String hi(){
+        return "welcome to use callService api!!!";
+    }
+
+    @GetMapping("/get")
+    public String callGetReq(@RequestParam(value = "schema", required = false) String schema,
+                             @RequestParam(value = "service", required = false) String service,
+                             @RequestParam(value = "path", required = false) String path) throws IOException {
+        log.info("callGetReq, schema: {}, service: {}, path: {}", schema, service, path);
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .get().url(String.format("%s:/%s/%s", schema, service, path))
+                .build();
+        Response response = client.newCall(request).execute();
+        String res = response.body().string();
+        log.info("rep: {}", res);
+        return res;
+    }
+}
--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.tencent.wxcloudrun.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * index控制器
  */
+@Slf4j
 @RestController
 public class IndexController {
 
@@ -18,6 +20,7 @@ public class IndexController {
    */
   @GetMapping("/index")
   public String index() {
+    log.info("index ===== ");
     String result = "欢迎使用云托管!服务版本：" + System.getenv("PUB_SERVICE_REVISION") + "\n实例主机：" + System.getenv("HOSTNAME") + "\n当前时间：" + LocalDateTime.now().format(
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     return result;
