e973f73b0ab92deb4a62d4b9441f1b2e3f0a77f3
huangkaifei <huangkaife.hkf@antgroup.com>
2023-04-13 16:07:01 +0800

    update index api

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.tencent.wxcloudrun.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * index控制器
  */
@@ -15,7 +18,9 @@ public class IndexController {
    */
   @GetMapping("/index")
   public String index() {
-    return "index";
+    String result = "欢迎使用云托管!服务版本：" + System.getenv("PUB_SERVICE_REVISION") + "\n实例主机：" + System.getenv("HOSTNAME") + "\n当前时间：" + LocalDateTime.now().format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    return result;
   }
 
 }
