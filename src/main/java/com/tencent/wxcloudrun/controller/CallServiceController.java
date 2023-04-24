85257c2e8f6992a705864f8eb6e8a16f89b2a9a1
huangkaifei <huangkaife.hkf@antgroup.com>
2023-04-24 10:08:48 +0800

    call service api

--- src/main/java/com/tencent/wxcloudrun/controller/CallServiceController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CallServiceController.java
@@ -4,13 +4,11 @@
  */
 package com.tencent.wxcloudrun.controller;
 
-import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
